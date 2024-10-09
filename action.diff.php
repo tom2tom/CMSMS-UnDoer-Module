e40711684cd3e24ce31f120cf79b834b72c6998b
tom2tom <tpgww@onepost.net>
2024-10-09 19:39:45 +1100

    placeholder action

---
+++ action.diff.php
@@ -0,0 +1,11 @@
+<?php
+/*
+This file is part of CMS Made Simple module: UnDoer
+Copyright (C) 2024 CMS Made Simple Foundation Inc <foundation@cmsmadesimple.org>
+Refer to license and other details at the top of file UnDoer.module.php
+*/
+
+if (empty($this) || !($this instanceof UnDoer)) exit;
+if (!$this->CheckPermission('Manage Restores')) return;
+
+echo("Not yet implemented");
